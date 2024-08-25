d79a6568cffc5d10164f8142a573d729e38c3554
DaBe1812 <d.becker1812@gmail.com>
2024-08-25 12:29:15 +0200

    Project changes for Issue

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.math.BigDecimal;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
 import java.util.ArrayList;
@@ -11,6 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import lombok.Data;
+import org.primefaces.event.RowEditEvent;
 
 @Data
 @Named
@@ -28,10 +31,23 @@ public class TestView implements Serializable {
         string = "Welcome to PrimeFaces!!!";
         list = new ArrayList<>(Arrays.asList(
                 new TestObject("Thriller", "Michael Jackson", 1982),
+                new TestObject("Scream", "Michael Jackson", 2017),
                 new TestObject("Back in Black", "AC/DC", 1980),
+                new TestObject("Powerup", "AC/DC", 2020),
                 new TestObject("The Bodyguard", "Whitney Houston", 1992),
-                new TestObject("The Dark Side of the Moon", "Pink Floyd", 1973)
+                new TestObject("The Dark Side of the Moon", "Pink Floyd", 1973),
+                new TestObject("The Endless River", "Pink Floyd", 2014)
         ));
+    }
+
+    public void onRowEdit(RowEditEvent<TestObject> event) {
+        FacesMessage msg = new FacesMessage("Product Edited", String.valueOf(event.getObject().getId()));
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public void onRowCancel(RowEditEvent<TestObject> event) {
+        FacesMessage msg = new FacesMessage("Edit Cancelled", String.valueOf(event.getObject().getId()));
+        FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 
 }
